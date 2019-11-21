a0c067bad599a8341fc2d9a8ada68371973ef5fc
Hariharan Venkata <hvenkata@roku.com>
2019-11-21 10:07:27 -0800

    Removed spaces

--- ci.sh
+++ ci.sh
@@ -4,5 +4,3 @@ set -e
 
 xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test
 xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS -configuration Release build CODE_SIGNING_ALLOWED=NO
-
-
